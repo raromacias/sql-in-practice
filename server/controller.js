b0097d5c026d2c4074ac9cd04bbdf4a429a0df50
Raro Macias <ggmacias@gmail.com>
2022-09-14 19:05:34 -0400

    done

--- server/controller.js
+++ server/controller.js
@@ -31,7 +31,10 @@ module.exports = {
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`*****YOUR CODE HERE*****
+        sequelize.query(`UPDATE cc_appointments AS a SET
+        a.approved = true,
+        WHERE apptId = ${apptId};
+        
         
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
@@ -54,5 +57,17 @@ module.exports = {
         WHERE a.approved = false ORDER by a.date desc;`)
         .then(dbRes => res.status(200).send(dbRes[0]))
         .catch(err => console.log(err))
+    },
+    getPastAppointments: (req, res) => {
+        sequelize.query(`SELECT a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
+        FROM cc_appointments AS a
+        JOIN cc_emp_appts AS ea ON a.appt_id = ea.appt_id
+        JOIN cc_employees AS e ON e.emp_id = ea.emp_id
+        JOIN cc_users AS u ON e.user_id = u.user_id
+        WHERE a.approved = true and a.completed = true
+        order by a.date desc;`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+
     }
 }
